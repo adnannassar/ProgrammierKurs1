38beb531013bf1c7b5ad08688787321574f9009a
Amer <aamb17447@gmail.com>
2023-04-07 17:11:35 +0200

    Amer Aufgabe001 erledigt

---
+++ src/Students/Amer/Aufgabe001/Bild.java
@@ -0,0 +1,25 @@
+package Students.Amer.Aufgabe001;
+
+public class Bild extends Medium {
+
+    private String ort;
+
+    public Bild(int jahr, String Titel, String ort) {
+        super(jahr, Titel);
+        this.ort=ort;
+    }
+
+
+    @Override
+    public void druckeDaten() {
+        System.out.println( "ID: "+getId()+" \""+getTitel()+ "\" aufgenommen im Jahr: "+getJahr()+" in \""+ort+"\"");
+
+    }
+
+    public String getOrt() {
+        return ort;
+    }
+
+
+}
+
---
+++ src/Students/Amer/Aufgabe001/Medium.java
@@ -0,0 +1,40 @@
+package Students.Amer.Aufgabe001;
+
+public abstract class Medium {
+    private int id;
+    private String Titel;
+    private int jahr;
+    static int cont=0;
+
+
+
+    public Medium (int jahr, String Titel){
+        this.jahr=jahr;
+        this.Titel=Titel;
+        id=cont;
+        cont++;
+    }
+
+
+    int x= java.time.LocalDate.now().getYear();
+
+    public int alter(){
+        return x-getJahr();
+
+    }
+    public abstract void druckeDaten();
+
+
+    public int getId() {
+        return id;
+    }
+
+    public int getJahr() {
+        return jahr;
+    }
+
+    public String getTitel() {
+        return Titel;
+    }
+}
+
