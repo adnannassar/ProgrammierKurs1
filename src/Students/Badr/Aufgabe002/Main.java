e236411da689f33ed71c086ead299eaf11521696
Bader969 <bader969@gmx.de>
2023-04-07 15:12:29 +0200

    changed Bader to Bader 123

--- src/Students/Badr/Aufgabe002/Main.java
+++ src/Students/Badr/Aufgabe002/Main.java
@@ -2,6 +2,6 @@ package Students.Badr.Aufgabe002;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Badr");
+        System.out.println("Badr123");
     }
 }
