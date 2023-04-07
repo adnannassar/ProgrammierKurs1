3a11efcb2c660b37a4439b15809899488b12a2ee
adnan <adnan.nassar90@gmail.com>
2023-04-07 12:37:31 +0200

    edited: aufgabe 2 by badr

--- src/Students/Badr/Aufgabe001/Main.java
+++ src/Students/Badr/Aufgabe001/Main.java
@@ -2,6 +2,6 @@ package Students.Badr.Aufgabe001;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Aufgabe 1 from Bard");
+        System.out.println("Aufgabe 2 from Bard");
     }
 }
