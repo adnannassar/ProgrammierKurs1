534d506f3ec1e9cdbe5a6a339ad7b2617882a480
Amer <aamb17447@gmail.com>
2023-04-17 12:58:04 +0200

    Amer Aufgabe001 erledigt

--- src/Students/AmerBathish/Main.java
+++ src/Students/AmerBathish/Main.java
@@ -1,5 +1,20 @@
-
 package Students.AmerBathish;
 
 public class Main {
+
+
+
+
+
+    public static void main(String[] args) {
+
+        Medium sp1 = new Audio(	1960,"hob Eh",2592,"umm kulthum");
+        Medium sp2 = new Bild(1506,"Mona Lisa","Paris");
+        sp1.druckeDaten();
+        sp2.druckeDaten();
+        System.out.println("\""+sp2.getTitel()+"\" ist "+sp2.alter()+" Jahre alt");
+        System.out.println("\""+sp1.getTitel()+"\" ist "+sp1.alter()+" Jahre alt");
+
+
+    }
 }
